93bb7e69af3fb5983a438e01f46c33aab3450f76
Timur <zairovne@gmail.com>
2022-11-21 18:30:09 +0500

    if else в handlebars как это работает и передача нужных элементов на страницу

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
-const {engine} = require("express-handlebars");
 
 const app = express()
 //config handlebars | layout and ext.name "hbs"
@@ -16,20 +15,33 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 
-//как использовать статические файлы
+//как использовать статические файлы например сss
 app.use(express.static('public'))
 
 
 //render pages
 app.get('/', (req, res) => {
     res.status(200)
-    res.render('index')
+        //вторым параметром добавляем для каждой страницы нужные нам элементы title и булен свойтсва для активных страниц
+    res.render('index', {
+        title: "Главная страница",
+        isHome: true
+    })
 })
 
-app.get('/about', (req, res) => {
-    res.render('about')
+app.get('/courses', (req, res) => {
+    res.render('courses', {
+        title: "Курсы",
+        isCourses: true
+    })
 })
 
+app.get('/add', (req, res) => {
+    res.render('add', {
+        title: "Добавить курс",
+        isAdd: true
+    })
+})
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
