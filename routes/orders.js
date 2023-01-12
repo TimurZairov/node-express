340273cbb02f01e5c03a530308ffd7f20092288e
Timur <zairovne@gmail.com>
2023-01-12 17:33:25 +0500

    Получение заказов у пользователя

--- routes/orders.js
+++ routes/orders.js
@@ -2,25 +2,46 @@ const {Router} = require('express')
 const Order = require('../models/order')
 const router = Router()
 
+
+//получаем покупки пользователя с базы данных
 router.get('/', async (req, res) => {
+    try {
+        const userOrders = await Order.find()
+        //новый объект для передачи на страницу пользователя (данные пользователя)
+        const user = {
+            name: req.user.name,
+            email: req.user.email,
+            date: userOrders[0].date,
+            price: userOrders[0].courses.reduce((acc, curr) => {
+                return acc = acc + curr.course.price * curr.count
+            }, 0),
+            id: userOrders[0]._id
+        }
 
-    const userOrders = await Order.find()
-    const orders = userOrders.filter(item => {
-        return item.user.userId.toString() === req.user._id.toString()
-    })
-    const courses = await orders?.map(item => {
-        return item.courses.map(item => {
-            return item.course
+        //новый массив для передачи на страницу пользователя (данные о купленных курсах)
+        const courses = []
+        const orders = userOrders.filter(item => {
+            return item.user.userId.toString() === req.user._id.toString()
         })
-    })
-
-    res.render('orders', {
-        isOrders: true,
-        title: 'Покупки',
-        courses
-    })
+        if(orders){
+            orders.map(item => {
+                item.courses.map(c => {
+                    courses.push(c.course)
+                })
+            })
+        }
+        res.render('orders', {
+            isOrders: true,
+            title: 'Покупки',
+            courses,
+            user
+        })
+    }catch (e) {
+        console.log(e)
+    }
 })
 
+//сохраняем купленные товары с корзины в orders
 router.post('/', async (req, res) => {
     try {
         //из пользователя корзины берем массив курсов
@@ -34,11 +55,11 @@ router.post('/', async (req, res) => {
             }
         })
         const order = new Order({
+            courses,
             user: {
                 name: req.user.name,
                 userId: req.user._id
-            },
-            courses
+            }
         })
         //сохраняем
         await order.save()
